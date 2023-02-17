ac217efd59c36dde7400a31d8b16c4f001f9f4a9
Khalid <elyemlahi98@gmail.com>
2023-02-16 17:11:02 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in c
- * code by bwave ict
- * Return: 0
+* main - print out sizes of data types in c
+* code by bwave ict
+* Return: 0
 */
 
 int main(void)
-{char a;  
+{
+char a;
 int b;
 long int c;
 long long int d;
