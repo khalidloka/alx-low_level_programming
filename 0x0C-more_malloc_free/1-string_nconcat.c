c73c2c139145d873cb18b4c34c2d0c62964e8363
Khalid <elyemlahi98@gmail.com>
2023-06-16 07:59:55 -0700

    add

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,8 +4,7 @@
 * *string_nconcat - concatenates n bytes of a string to another string
 * @s1: string to append to
 * @s2: string to concatenate from
-
-* @n: number of bytes from s2 to concatenate to s1
+*@n: number of bytes from s2 to concatenate to s1
 *
 * Return: pointer to the resulting string
 */
