ee5a252b99ee549eb9b7314da0e8c4b44659b54a
Khalid <elyemlahi98@gmail.com>
2023-07-08 05:40:54 -0700

    sdd

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,24 @@
-#include "main.h"
+nary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+#ilude "main.h"
 
 /**
  * print_binary - prints the binary equivalent of a decimal number
@@ -6,18 +26,22 @@
  */
 void print_binary(unsigned long int n)
 {
-int i, count = 0;
-unsigned long int current;
-for (i = 63; i >= 0; i--)
-{
-current = n >> i;
-if (current & 1)
-_putchar('1');
-count++;
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
-else if (count)
-_putchar('0');
-}
-if (!count)
-_putchar('0');
-}
+
